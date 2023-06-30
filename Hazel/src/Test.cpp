032519d9a80ab30abd379f9d4475ff611d445100
Wesley Fortin <wfortin@uwo.ca>
2023-06-30 00:22:05 -0400

    Initial Project Setup

---
+++ Hazel/src/Test.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Test.h"
+
+#include <iostream>
+
+namespace Hazel {
+	
+	void Print()
+	{
+		std::cout << "Welcome to Hazel Engine!" << std::endl; 
+	}
+
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,9 @@
+namespace Hazel {
+	__declspec(dllimport) void Print(); 
+}
+
+int main()
+{
+	Hazel::Print(); 
+	return 0; 
+}
